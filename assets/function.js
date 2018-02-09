0785ba1a8a3626d361866c2b17c6f82d1672dd11
davidh6164 <davidh882@gmail.com>
2018-02-09 12:27:35 -0700

    Add files via upload

--- assets/function.js
+++ assets/function.js
@@ -1,5 +1,19 @@
-ClipPath('.col-clip', '0 0, 100% 0%, 100% 59%, 0% 100%');
-
+$(document).ready(function() {
+  ClipPath('.col-clip', '0 0, 100% 0%, 100% 59%, 0% 100%');
+});
+$(document).ready(function() {
 ClipPath('.col-clip2', '0 59%, 100% 0%, 100% 100%, 0% 100%');
-
+});
+$(document).ready(function() {
 ClipPath('.col-clip3', '0 59%, 100% 0%, 100% 100%, 0% 100%');
+});
+
+$(window).scroll(function () {
+    var $this = $(this),
+        $top = $('html');
+    if ($this.scrollTop() > 700) {
+       $head.addClass('slide-In');
+    } else {
+       $head.removeClass('slide-In');
+    }
+});
